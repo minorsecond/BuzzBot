59593b41b6f5f5494dd91fe839c3a873533cd061
Ross Wardrup <minorsecond@gmail.com>
2020-09-10 11:38:01 -0500

    Write drink calculation functions and tests

---
+++ src/calculate.h
@@ -0,0 +1,22 @@
+//
+// Created by Ross Wardrup on 9/10/20.
+//
+
+#ifndef BEERTABS_CALCULATE_H
+#define BEERTABS_CALCULATE_H
+
+#include <string>
+
+
+class Calculate {
+public:
+    static double oz_alcohol(double abv, int amount);
+    static double standard_drinks(double abv, int amount);
+    static double standard_drinks_remaining(const std::string& sex, double standard_drinks_consumed);
+
+private:
+    static double round_to_one_decimal_point(double val);
+};
+
+
+#endif //BEERTABS_CALCULATE_H
