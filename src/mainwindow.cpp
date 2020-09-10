764975b5e10c986604be5f8e3f7f779320816f43
Ross Wardrup <minorsecond@gmail.com>
2020-09-10 15:39:38 -0500

    Adjust application width

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -18,7 +18,7 @@ MainWindow::MainWindow(QWidget *parent)
      */
 
     ui->setupUi(this);
-    this->setFixedSize(1393, 665);
+    this->setFixedSize(1392, 665);
 
     // Read program options
     std::cout << "Got sex: " << sex << std::endl;
