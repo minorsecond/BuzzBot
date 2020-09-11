f65dcae2cb38ac7b04c6016508916b01deb63a00
Ross Wardrup <minorsecond@gmail.com>
2020-09-11 08:38:50 -0500

    Write tests for stats calculation code

--- src/calculate.h
+++ src/calculate.h
@@ -14,10 +14,10 @@ public:
     static double oz_alcohol(double abv, int amount);
     static double standard_drinks(double abv, int amount);
     static double standard_drinks_remaining(const std::string& sex, double standard_drinks_consumed);
-    static double oz_alcohol_remaining(const std::string& sex, double oz_consumed);  //TODO write test
+    static double oz_alcohol_remaining(const std::string& sex, double oz_consumed);
     static double round_to_one_decimal_point(double val);
-    static std::string favorite_brewery(Storage storage);  // TODO write test
-    static std::string favorite_beer(Storage storage);  // TODO write test
+    static std::string favorite_brewery(Storage storage);
+    static std::string favorite_beer(Storage storage);
     static double mean_abv(Storage storage);  // TODO write test
     static double mean_ibu(Storage storage);  // TODO write test
 };
