14c3d9b05dc43c17334f0cbefbff8f0a737998e6
Ross Wardrup <minorsecond@gmail.com>
2020-09-16 10:16:36 -0500

    Dynamically set version number

--- src/about.cpp
+++ src/about.cpp
@@ -11,6 +11,12 @@ About::About(QWidget *parent) {
 
     ui.setupUi(this);
     this->setFixedSize(230, 252);
+
+    // Version text
+    std::string version = "0.8.0";
+    ui.versionLabel->setText(QString::fromStdString("Version " + version));
+
     QPixmap pixmap("../Resources/mini-icon.png");
     ui.miniIcon->setPixmap(pixmap);
+    ui.miniIcon->repaint();
 }
