02d285fea8733c26c5e4cf614d1b1d7424bf61b8
Ross Wardrup <minorsecond@gmail.com>
2020-09-17 06:54:26 -0500

    Fix increment_vers.sh

--- increment_vers.sh
+++ increment_vers.sh
@@ -1,7 +1,7 @@
-#!/bin/zh
+#!/bin/zsh
 
 echo "INCREMENTING TO VERSION " $1
 
-sed 's/\(MACOSX_BUNDLE_SHORT_VERSION_STRING \)\(.*\)/\1$1/' CMakeLists.txt
-sed 's/\(MACOSX_BUNDLE_LONG_VERSION_STRING \)\(.*\)/\1$1/' CMakeLists.txt
-sed 's/\(std::string version = \)\(.*\)/\1$1/' src/about.cpp
+sed 's/\(MACOSX_BUNDLE_SHORT_VERSION_STRING \)\(.*\)/\1'$1'/' CMakeLists.txt
+sed 's/\(MACOSX_BUNDLE_LONG_VERSION_STRING \)\(.*\)/\1'$1'/' CMakeLists.txt
+sed 's/\(std::string version = \)\(.*\)/\1'$1'/' src/about.cpp
