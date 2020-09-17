8f3d8aebd3b719befbed5c56ecdb24bee1805f75
Ross Wardrup <minorsecond@gmail.com>
2020-09-17 12:36:50 -0500

    Hardened codesigning

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 echo "Copying Beertabs.app to desktop."
+rm -R ~/Desktop/Beertabs.app
 cp -R cmake-build-release/Beertabs.app ~/Desktop/Beertabs.app
 
 echo "Running macdeployqt."
@@ -9,6 +10,6 @@ macdeployqt ~/Desktop/Beertabs.app -appstore-compliant
 echo "Codesigning."
 find ~/Desktop/Beertabs.app -name "*.dylib" | xargs -I $ codesign --verify --verbose --sign $1 $
 find ~/Desktop/Beertabs.app -name "*.framework" | xargs -I $ codesign --verify --verbose --sign $1 $
-codesign --sign $1 --verbose --entitlements Beertabs-Entitlements.plist ~/Desktop/Beertabs.app
+codesign --sign $1 --options runtime --verbose --entitlements Beertabs-Entitlements.plist ~/Desktop/Beertabs.app
 
 echo "Deployed to ~/Desktop/Beertabs.app"
