f9ceea6b97a13caf2abcc2403a32ada2fa8726f8
Ross Wardrup <minorsecond@gmail.com>
2020-09-17 12:56:38 -0500

    Harden libraries

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ echo "Running macdeployqt."
 macdeployqt ~/Desktop/Beertabs.app -appstore-compliant
 
 echo "Codesigning."
-find ~/Desktop/Beertabs.app -name "*.dylib" | xargs -I $ codesign --verify --verbose --sign $1 $
-find ~/Desktop/Beertabs.app -name "*.framework" | xargs -I $ codesign --verify --verbose --sign $1 $
+find ~/Desktop/Beertabs.app -name "*.dylib" | xargs -I $ codesign --options runtime --verify --verbose --sign $1 $
+find ~/Desktop/Beertabs.app -name "*.framework" | xargs -I $ codesign --options runtime --verify --verbose --sign $1 $
 codesign --sign $1 --options runtime --verbose --entitlements Beertabs-Entitlements.plist ~/Desktop/Beertabs.app
 
 echo "Deployed to ~/Desktop/Beertabs.app"
