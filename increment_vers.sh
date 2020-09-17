a5f7312ac8775902b64a7130d64002e14a7b5512
Ross Wardrup <minorsecond@gmail.com>
2020-09-17 06:56:41 -0500

    Write to file

--- increment_vers.sh
+++ increment_vers.sh
@@ -2,6 +2,6 @@
 
 echo "INCREMENTING TO VERSION " $1
 
-sed 's/\(MACOSX_BUNDLE_SHORT_VERSION_STRING \)\(.*\)/\1'$1'/' CMakeLists.txt
-sed 's/\(MACOSX_BUNDLE_LONG_VERSION_STRING \)\(.*\)/\1'$1'/' CMakeLists.txt
-sed 's/\(std::string version = \)\(.*\)/\1'$1'/' src/about.cpp
+sed -i 's/\(MACOSX_BUNDLE_SHORT_VERSION_STRING \)\(.*\)/\1'$1'/' CMakeLists.txt
+sed -i 's/\(MACOSX_BUNDLE_LONG_VERSION_STRING \)\(.*\)/\1'$1'/' CMakeLists.txt
+sed -i 's/\(std::string version = \)\(.*\)/\1'$1'/' src/about.cpp
