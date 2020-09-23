e78273db85e10351d08917656482da76bd841c3b
Ross Wardrup <minorsecond@gmail.com>
2020-09-23 13:34:06 -0500

    Don't skip second if statement and add coverage

--- coverage.sh
+++ coverage.sh
@@ -5,6 +5,7 @@
 #
 
 mkdir ~/Desktop/BuzzBot-Coverage/
+rm -rvf ~/Desktop/BuzzBot-Coverage/*
 lcov --capture --directory . --no-external -o ~/Desktop/BuzzBot-Coverage/coverage.info
 lcov -r ~/Desktop/BuzzBot-Coverage/coverage.info "*include*" "*cmake-build*" "*.h" -o ~/Desktop/BuzzBot-Coverage/final_coverage.info
 genhtml ~/Desktop/BuzzBot-Coverage/final_coverage.info --output-directory ~/Desktop/BuzzBot-Coverage/
