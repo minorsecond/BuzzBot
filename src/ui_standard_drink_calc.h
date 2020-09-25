8e81bdbe2e4904bd83a2ad2a39356b7976845f4f
Ross Wardrup <minorsecond@gmail.com>
2020-09-25 10:17:08 -0500

    Add standard drink calc GUI

---
+++ src/ui_standard_drink_calc.h
@@ -0,0 +1,104 @@
+/********************************************************************************
+** Form generated from reading UI file 'std_drink_dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_STANDARD_DRINK_CALC_H
+#define UI_STANDARD_DRINK_CALC_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QDoubleSpinBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QSpinBox>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_stdDrinkDialog
+{
+public:
+    QGridLayout *gridLayout;
+    QLabel *volumeLabel;
+    QLabel *abvInputLabel;
+    QDoubleSpinBox *abvInput;
+    QDialogButtonBox *standardDrinkCalcButtonBox;
+    QSpinBox *volumeInput;
+    QLabel *standardDrinksLabel;
+    QLabel *standardDrinksOutput;
+
+    void setupUi(QDialog *stdDrinkDialog)
+    {
+        if (stdDrinkDialog->objectName().isEmpty())
+            stdDrinkDialog->setObjectName(QString::fromUtf8("stdDrinkDialog"));
+        stdDrinkDialog->resize(230, 142);
+        gridLayout = new QGridLayout(stdDrinkDialog);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        volumeLabel = new QLabel(stdDrinkDialog);
+        volumeLabel->setObjectName(QString::fromUtf8("volumeLabel"));
+
+        gridLayout->addWidget(volumeLabel, 1, 0, 1, 1);
+
+        abvInputLabel = new QLabel(stdDrinkDialog);
+        abvInputLabel->setObjectName(QString::fromUtf8("abvInputLabel"));
+
+        gridLayout->addWidget(abvInputLabel, 0, 0, 1, 1);
+
+        abvInput = new QDoubleSpinBox(stdDrinkDialog);
+        abvInput->setObjectName(QString::fromUtf8("abvInput"));
+
+        gridLayout->addWidget(abvInput, 0, 1, 1, 1);
+
+        standardDrinkCalcButtonBox = new QDialogButtonBox(stdDrinkDialog);
+        standardDrinkCalcButtonBox->setObjectName(QString::fromUtf8("standardDrinkCalcButtonBox"));
+        standardDrinkCalcButtonBox->setOrientation(Qt::Horizontal);
+        standardDrinkCalcButtonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+
+        gridLayout->addWidget(standardDrinkCalcButtonBox, 3, 0, 1, 2);
+
+        volumeInput = new QSpinBox(stdDrinkDialog);
+        volumeInput->setObjectName(QString::fromUtf8("volumeInput"));
+
+        gridLayout->addWidget(volumeInput, 1, 1, 1, 1);
+
+        standardDrinksLabel = new QLabel(stdDrinkDialog);
+        standardDrinksLabel->setObjectName(QString::fromUtf8("standardDrinksLabel"));
+
+        gridLayout->addWidget(standardDrinksLabel, 2, 0, 1, 1);
+
+        standardDrinksOutput = new QLabel(stdDrinkDialog);
+        standardDrinksOutput->setObjectName(QString::fromUtf8("standardDrinksOutput"));
+
+        gridLayout->addWidget(standardDrinksOutput, 2, 1, 1, 1);
+
+
+        retranslateUi(stdDrinkDialog);
+        QObject::connect(standardDrinkCalcButtonBox, SIGNAL(accepted()), stdDrinkDialog, SLOT(accept()));
+        QObject::connect(standardDrinkCalcButtonBox, SIGNAL(rejected()), stdDrinkDialog, SLOT(reject()));
+
+        QMetaObject::connectSlotsByName(stdDrinkDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *stdDrinkDialog)
+    {
+        stdDrinkDialog->setWindowTitle(QCoreApplication::translate("stdDrinkDialog", "Calculate Std. Drinks", nullptr));
+        volumeLabel->setText(QCoreApplication::translate("stdDrinkDialog", "Volume:", nullptr));
+        abvInputLabel->setText(QCoreApplication::translate("stdDrinkDialog", "ABV:", nullptr));
+        standardDrinksLabel->setText(QCoreApplication::translate("stdDrinkDialog", "Standard drinks:", nullptr));
+        standardDrinksOutput->setText(QCoreApplication::translate("stdDrinkDialog", "10", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class stdDrinkDialog: public Ui_stdDrinkDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_STANDARD_DRINK_CALC_H
