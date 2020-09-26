e346bc59df6fcb555feb400908e052b71321c133
Ross Wardrup <minorsecond@gmail.com>
2020-09-26 05:11:30 -0500

    Improved vers incrementer & incremented version

--- increment_vers.sh
+++ increment_vers.sh
@@ -6,6 +6,7 @@
 #
 
 # Parse date
+VERS_LINE=$(tail -n 1 VERS)
 PREV_VER=$(tail -n 1 VERS | awk '{print $1;}')
 DATE=$(date +"%Y-%m-%d-%H-%M-%S")
 
@@ -19,10 +20,12 @@ NEW_MAJ=`echo $1 | cut -d. -f1`
 NEW_MINOR=`echo $1 | cut -d. -f2`
 NEW_PATCH=`echo $1 | cut -d. -f3`
 
+NEW_VERS="$1"
+
 # Check if it's possible to increment version
 if [[ $PREV_MAJ -lt $NEW_MAJ || $PREV_MINOR -lt $NEW_MINOR || $PREV_PATCH -lt $NEW_PATCH ]];
 then
-  echo "Incrementing to version " \"$@\"
+  echo "Incrementing to version " "$1"
   sed -i '' -e 's/\(MACOSX_BUNDLE_SHORT_VERSION_STRING \)\(.*\)/\1'\"$1\"'/' CMakeLists.txt
   sed -i '' -e 's/\(MACOSX_BUNDLE_LONG_VERSION_STRING \)\(.*\)/\1'\"$1\"'/' CMakeLists.txt
   sed -i '' -e 's/\(MACOSX_BUNDLE_BUNDLE_VERSION \)\(.*\)/\1'\"$1\"'/' CMakeLists.txt
@@ -32,6 +35,20 @@ then
 
   echo "Incremented version " $PREV_VER " to " $1
   echo "Refresh CMake to apply changes to Info.plist."
+
+elif [[ $PREV_MAJ -eq $NEW_MAJ && $PREV_MINOR -eq $NEW_MINOR && $PREV_PATCH -eq $NEW_PATCH && $NEW_VERS != *"beta"* && $VERS_LINE == *"beta"* ]]
+then
+  echo "Upgrading from beta version to " "$1"
+  sed -i '' -e 's/\(MACOSX_BUNDLE_SHORT_VERSION_STRING \)\(.*\)/\1'\"$1\"'/' CMakeLists.txt
+  sed -i '' -e 's/\(MACOSX_BUNDLE_LONG_VERSION_STRING \)\(.*\)/\1'\"$1\"'/' CMakeLists.txt
+  sed -i '' -e 's/\(MACOSX_BUNDLE_BUNDLE_VERSION \)\(.*\)/\1'\"$1\"'/' CMakeLists.txt
+  sed -i '' -e 's/\(std::string version = \)\(.*\)/\1'\"$1\"\;'/' src/about.cpp
+
+  echo "$1" $DATE >> VERS
+
+  echo "Incremented version " "$PREV_VER" " to " "$1"
+  echo "Refresh CMake to apply changes to Info.plist."
+
 elif [[ $PREV_MAJ -eq $NEW_MAJ && $PREV_MINOR -eq $NEW_MINOR && $PREV_PATCH -eq $NEW_PATCH ]];
 then
   echo "Versions are the same. Nothing to do."
