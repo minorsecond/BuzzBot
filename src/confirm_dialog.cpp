b286340ade3fc9abe62d704e67b572836babf251
Ross Wardrup <minorsecond@gmail.com>
2020-09-29 08:21:18 -0500

    Confirm before deleting a row

--- src/confirm_dialog.cpp
+++ src/confirm_dialog.cpp
@@ -4,12 +4,14 @@
 
 #include "confirm_dialog.h"
 
-ConfirmDialog::ConfirmDialog(QWidget *parent, std::string action) {
+ConfirmDialog::ConfirmDialog(QWidget *parent, const std::string& action) {
     /*
      * Delete/edit confirmation dialog.
      * @param parent: Parent QtWidget.
      * @param action: String denoting either edit or delete. This changes the display text.
      */
+
+    ui.setupUi(this);
 
     if (action == "Delete") {
         ui.actionConfirmationLabel->setText("You are attempting to delete a row. Click OK to confirm.");
