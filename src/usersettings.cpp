70dbcd06680fa2df3e063cc09375908529632522
Ross Wardrup <minorsecond@gmail.com>
2020-10-01 08:25:53 -0500

    Add custom limit option to user_settings dialog box

--- src/usersettings.cpp
+++ src/usersettings.cpp
@@ -12,7 +12,7 @@ UserSettings::UserSettings(QWidget *parent, const Options& options) {
      */
 
     ui.setupUi(this);
-    this->setFixedSize(422, 195);
+    this->setFixedSize(615, 195);
 
     if (options.sex == "male") {
         ui.maleSelection->setChecked(true);
