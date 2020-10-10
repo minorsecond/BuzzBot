3942dedb676a01e1f85c8196f1b6e649307126f1
Ross Wardrup <minorsecond@gmail.com>
2020-10-10 07:13:21 -0500

    Code cleanup, increment version

--- src/about.cpp
+++ src/about.cpp
@@ -16,7 +16,7 @@ About::About(QWidget *parent) {
     this->setFixedSize(237, 272);
 
     // Version text
-    std::string version = "1.0.6";
+    std::string version = "1.1.0-beta";
     ui.versionLabel->setText(QString::fromStdString("Version " + version));
 
     // Make Qt URL clickable
