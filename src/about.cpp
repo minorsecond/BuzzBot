d261c911a815e8942a934bf5a3058b195f9a30aa
Ross Wardrup <minorsecond@gmail.com>
2020-12-03 03:04:19 -0600

    Increment version to 1.2.0

--- src/about.cpp
+++ src/about.cpp
@@ -16,7 +16,7 @@ About::About(QWidget *parent) {
     this->setFixedSize(237, 272);
 
     // Version text
-    std::string version = "1.2.0-beta";
+    std::string version = "1.2.0";
     ui.versionLabel->setText(QString::fromStdString("Version " + version));
 
     // Make Qt URL clickable
