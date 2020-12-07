e29012033eccf1f431fb8f959f03e3725b6b9096
Ross Wardrup <minorsecond@gmail.com>
2020-12-07 10:49:49 -0600

    Add graphing class

---
+++ src/graphing.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ross Wardrup on 12/7/20.
+//
+
+#include "graphing.h"
---
+++ src/graphing.h
@@ -0,0 +1,14 @@
+//
+// Created by Ross Wardrup on 12/7/20.
+//
+
+#ifndef BUZZBOT_GRAPHING_H
+#define BUZZBOT_GRAPHING_H
+
+
+class graphing {
+
+};
+
+
+#endif //BUZZBOT_GRAPHING_H
