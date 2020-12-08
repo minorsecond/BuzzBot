ec014188a9f50a265061ab6a8ad00acd4a3cfe6d
Ross Wardrup <minorsecond@gmail.com>
2020-12-08 08:43:02 -0600

    Update to Qt6

--- src/ui_standard_drink_calc.h
+++ src/ui_standard_drink_calc.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'standard_drink_calc.ui'
 **
-** Created by: Qt User Interface Compiler version 5.15.0
+** Created by: Qt User Interface Compiler version 6.0.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
