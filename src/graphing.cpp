7873cf9a9912ba6dd243005ec11fc0d582b66d0a
Ross Wardrup <minorsecond@gmail.com>
2021-04-20 15:45:49 -0600

    Get beer IBUs and all drink ABVs

--- src/graphing.cpp
+++ src/graphing.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "graphing.h"
+
+std::vector<double> graphing::get_beer_ibus(const std::vector<Drink>& all_drinks) {
+    /*
+     * Create a vector containing IBU values of all beers.
+     * @param all_drinks: A vector of drinks. This will be all drinks in the database.
+     * @return: Vector of doubles representing all beer IBUs.
+     */
+
+    std::vector<double> ibu_values;
+
+    for (auto& drink : all_drinks) {
+
+        double ibu = drink.ibu;
+        if (ibu > 0)
+        ibu_values.push_back(ibu);
+    }
+
+    return ibu_values;
+}
+
+std::vector<double> graphing::get_beer_abvs(const std::vector<Drink> &all_drinks) {
+    /*
+     * Create a vector containing ABV values of all drinks.
+     * @param all_drinks: A vector of drinks. This will be all drinks in the database.
+     * @return: A vector of doubles denoting all drink IBUs.
+     */
+
+    std::vector<double> abv_values(all_drinks.size());
+
+    for (auto& drink : all_drinks) {
+        abv_values.push_back(drink.abv);
+    }
+
+    return abv_values;
+}
--- src/graphing.h
+++ src/graphing.h
@@ -5,9 +5,14 @@
 #ifndef BUZZBOT_GRAPHING_H
 #define BUZZBOT_GRAPHING_H
 
+#include "database.h"
+
 
 class graphing {
 
+private:
+    std::vector<double> get_beer_ibus(const std::vector<Drink>& all_drinks);
+    std::vector<double> get_beer_abvs(const std::vector<Drink>& all_drinks);
 };
 
 
