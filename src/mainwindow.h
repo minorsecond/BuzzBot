35d6a10187e0a93062a97f74f7b5ff3dd2561865
Ross Wardrup <minorsecond@gmail.com>
2021-04-23 07:35:37 -0600

    Remove unecessary references to beer in mainwindow.cpp

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -38,10 +38,10 @@ private:
     void program_options(bool write);
     void update_drinks_this_week(double standard_drinks, const std::string& weekday_name);
     void update_standard_drinks_left_this_week(double std_drinks_consumed);
-    double update_vol_alcohol_consumed_this_week(const std::vector<Drink>& beers_this_week, const std::string& weekday_name);
+    double update_vol_alcohol_consumed_this_week(const std::vector<Drink>& drinks_this_week, const std::string& weekday_name);
     void update_volume_alcohol_remaining(double volume_alcohol_consumed);
-    void update_favorite_brewery(const std::string& drink_type);
-    void update_favorite_beer(const std::string& drink_type);
+    void update_favorite_producer(const std::string& drink_type);
+    void update_favorite_drink(const std::string& drink_type);
     void update_favorite_type(const std::string& drink_type);
     void update_mean_abv(const std::string& drink_type);
     void update_mean_ibu(const std::string& drink_type);
