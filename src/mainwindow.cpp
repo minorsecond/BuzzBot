338ad0f8cbb350a0ab8bb5e9de9fa9427a23d5f5
Ross Wardrup <minorsecond@gmail.com>
2021-04-24 06:07:27 -0600

    Refactor local date method

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -682,19 +682,15 @@ std::tuple<date::year_month_day, std::string> MainWindow::get_filter_date() {
 
     date::weekday filter_day = get_filter_weekday_start();
 
-    // Get today's date in local time.
-    auto todays_date = std::chrono::system_clock::now();
-    auto now_c = std::chrono::system_clock::to_time_t(todays_date);
-    std::tm now_tm = *std::localtime(&now_c);
-    char query_date[70];
-    std::strftime(query_date, sizeof query_date, "%Y-%m-%d", &now_tm);
-    std::cout << "The time is " << query_date << std::endl;
+    std::string query_date = get_local_date();
 
     // Back to date object
     std::tm tm = {};
     std::stringstream ss(query_date);
     ss >> std::get_time(&tm,"%Y-%m-%d");
     auto tp = date::floor<date::days>(std::chrono::system_clock::from_time_t(std::mktime(&tm)));
+
+    std::cout << "The system time is: " << date::format("%Y-%m-%d %H-%M-%S", tp) << std::endl;
 
     // Get date of last filter_day
     if (options.date_calculation_method == "Fixed") {
@@ -738,4 +734,29 @@ std::string MainWindow::format_date(date::year_month_day date) {
     std::string query_date = year + "-" + month + "-" + day;
 
     return query_date;
+}
+
+std::string MainWindow::get_local_date() {
+    /*
+     * Get date in localtime
+     * @return: A string denoting the date in localtime, with the format YYYY-MM-DD
+     */
+
+    // Get today's date in local time.
+    char query_date[10];
+    std::string output;
+
+    auto todays_date = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(todays_date);
+    std::tm now_tm = *std::localtime(&now_c);
+    std::strftime(query_date, sizeof query_date, "%Y-%m-%d", &now_tm);
+
+    for (char i : query_date) {
+        output += std::string(1, i);
+        std::cout << i << std::endl;
+    }
+
+    std::cout << "The time is " << output << std::endl;
+
+    return output;
 }
