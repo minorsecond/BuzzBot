89bdb95322a9da062a78069cea0b8f9a9cfcd5bd
Ross Wardrup <minorsecond@gmail.com>
2021-08-19 13:41:20 -0600

    Pass storage by ref in get_drink_by_name

--- src/database.h
+++ src/database.h
@@ -65,8 +65,8 @@ public:
     static void update(Storage storage, const Drink& drink);
     static std::vector<Drink> filter(const std::string& filter_type, const std::string& filter_text, Storage storage);
     static void write_db_to_disk(Storage &storage);
-    static Drink get_drink_by_name(Storage storage, const std::string &alcohol_type, const std::string &drink_name);
-    static Drink get_drink_by_name(Storage storage, const std::string &alcohol_type, const std::string &drink_name, const std::string &producer);
+    static Drink get_drink_by_name(Storage &storage, const std::string &alcohol_type, const std::string &drink_name);
+    static Drink get_drink_by_name(Storage &storage, const std::string &alcohol_type, const std::string &drink_name, const std::string &producer);
     static std::vector<Drink> get_drinks_by_type(Storage storage, std::string drink_type);
     static std::vector<Drink> get_drinks_by_producer(Storage storage, std::string producer);
     static int get_version(Storage storage);
