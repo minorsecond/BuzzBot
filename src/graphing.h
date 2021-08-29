2c8691f072913ae6714b81fcf351b8d852805296
Ross Wardrup <minorsecond@gmail.com>
2021-08-29 13:13:23 -0600

    Use pass by value instead of manual copying vector

--- src/graphing.h
+++ src/graphing.h
@@ -25,7 +25,7 @@ public:
     static std::vector<double> get_drink_abvs(const std::vector<Drink>& all_drinks);
     static std::map<double, int> count_values_in_vect(const std::vector<double>& all_values);
     static QCustomPlot * plot_ibus(const std::map<double, int>& ibu_counts, QDialog *parent = nullptr);
-    static QVector<QCPGraphData> time_data_aggregator(const std::vector<Drink>& all_drinks, double std_drink_size);
+    static QVector<QCPGraphData> time_data_aggregator(std::vector<Drink> all_drinks, double std_drink_size);
     static bool compare_by_date(const Drink &a, const Drink &b);
     static int parse_date(std::string &date);
     static QCustomPlot * plot_abvs(const QVector<QCPGraphData>& time_data, const Options& options, QDialog *parent);
