dc799eca4675a3a82991da4e4bca5ee8109f5b9b
Ross Wardrup <minorsecond@gmail.com>
2021-11-01 10:09:49 -0600

    Increment version to 2.2.0

--- src/about.cpp
+++ src/about.cpp
@@ -15,7 +15,7 @@ About::About() {
     this->setFixedSize(237, 272);
 
     // Version text
-    std::string version = "2.1.7";
+    std::string version = "2.2.0";
     ui.versionLabel->setText(QString::fromStdString("Version " + version));
 
     ui.copyrightLabel->setText("Ross Wardrup, 2021");
