64ce905e5a51dfcdfcf22522a5368737d123ebc1
Ross Wardrup <minorsecond@gmail.com>
2022-09-21 07:53:03 -0600

    Finish up refactoring
    #412

---
+++ src/drink_standards.cpp
@@ -0,0 +1,32 @@
+//
+// Created by rwardrup on 9/21/22.
+//
+
+#include "drink_standards.h"
+
+// Std drink sizes are all stored in Oz. Data are all from Wikipedia:
+// https://en.wikipedia.org/wiki/Standard_drink
+std::map<std::string , double> std_drink_standards = {
+        {"Australia", 0.43},
+        {"Austria", 0.86},
+        {"Canada", 0.57},
+        {"Denmark", 0.52},
+        {"Finland", 0.52},
+        {"France", 0.43},
+        {"Germany", 0.47},
+        {"Hong Kong", 0.43},
+        {"Hungary", 0.73},
+        {"Iceland", 0.34},
+        {"Ireland", 0.43},
+        {"Italy", 0.43},
+        {"Japan", 0.85},
+        {"Netherlands", 0.43},
+        {"New Zealand", 0.43},
+        {"Poland", 0.43},
+        {"Portugal", 0.47},
+        {"Spain", 0.43},
+        {"Sweden", 0.51},
+        {"Switzerland", 0.52},
+        {"United Kingdom", 0.34},
+        {"United States", 0.60}
+};
--- src/drink_standards.h
+++ src/drink_standards.h
@@ -5,31 +5,9 @@
 #ifndef BUZZBOT_DRINK_STANDARDS_H
 #define BUZZBOT_DRINK_STANDARDS_H
 
-// Std drink sizes are all stored in Oz. Data are all from Wikipedia:
-// https://en.wikipedia.org/wiki/Standard_drink
-std::map<std::string , double> std_drink_standards = {
-        {"Australia", 0.43},
-        {"Austria", 0.86},
-        {"Canada", 0.57},
-        {"Denmark", 0.52},
-        {"Finland", 0.52},
-        {"France", 0.43},
-        {"Germany", 0.47},
-        {"Hong Kong", 0.43},
-        {"Hungary", 0.73},
-        {"Iceland", 0.34},
-        {"Ireland", 0.43},
-        {"Italy", 0.43},
-        {"Japan", 0.85},
-        {"Netherlands", 0.43},
-        {"New Zealand", 0.43},
-        {"Poland", 0.43},
-        {"Portugal", 0.47},
-        {"Spain", 0.43},
-        {"Sweden", 0.51},
-        {"Switzerland", 0.52},
-        {"United Kingdom", 0.34},
-        {"United States", 0.60}
-};
+#include <string>
+#include <map>
+
+extern std::map<std::string , double> std_drink_standards;
 
 #endif //BUZZBOT_DRINK_STANDARDS_H
