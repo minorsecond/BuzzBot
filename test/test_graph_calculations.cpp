c353228a9403eafd1f8f1022b0c5eeac558e9155
Ross Wardrup <minorsecond@gmail.com>
2022-09-23 13:19:16 -0600

    BuzzBot-406 Fix floating point comparisons in test

--- test/test_graph_calculations.cpp
+++ test/test_graph_calculations.cpp
@@ -19,7 +19,7 @@ TEST_CASE("IBU Vector Creation", "[Graph Data Compilation]") {
     Drink etrwo2;
     Drink titos;
 
-    etrwo.set_id(-1); // 1
+    etrwo.set_id(-1);
     etrwo.set_date("2020-09-08");
     etrwo.set_name("Everything Rhymes with Orange");
     etrwo.set_type("IPA");
