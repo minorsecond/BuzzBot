ec4af88cd4fe80abc72ecf332a9fc9a34aee8aea
Ross Wardrup <minorsecond@gmail.com>
2022-10-07 11:19:51 -0500

    Use enum for confirmation dialog parameter

--- src/usersettings.cpp
+++ src/usersettings.cpp
@@ -184,7 +184,7 @@ void UserSettings::clicked_clear_data() {
      * Clear the data if user so desires.
      */
 
-    ConfirmDialog confirmation_dialog = ConfirmDialog("Clear Data");
+    ConfirmDialog confirmation_dialog = ConfirmDialog(ConfirmStatus::ClearData);
     if (confirmation_dialog.exec() == QDialog::Accepted) {
         Storage storage = initStorage(utilities::get_db_path());
         Database::truncate(storage);
@@ -409,7 +409,7 @@ void UserSettings::clicked_browse_db_path() {
      * @return: None
      */
 
-    ConfirmDialog change_db_path_confirm("Moving DB");
+    ConfirmDialog change_db_path_confirm(ConfirmStatus::MovingDb);
 
     if (change_db_path_confirm.exec() == QDialog::Accepted) {
         const QString desktop_path = QStandardPaths::standardLocations(QStandardPaths::DesktopLocation).at(0);
